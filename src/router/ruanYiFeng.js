6ced17475c1c64a54f129ac9cdea5c78114ea173
xiehuaqiang <mrxiehuaqiang@163.com>
2020-09-21 18:27:01 +0800

    增加阮一峰的书籍

---
+++ src/router/ruanYiFeng.js
@@ -0,0 +1,15 @@
+const RouterView = () => import('components/RouterView')
+
+const component = () => import(/* webpackChunkName: "ruanYiFeng" */ '@/pages/bookViewList')
+
+const ruanYiFeng = {
+  path: '/ruanyifeng',
+  name: "ECMAScript 6",
+  component: RouterView,
+  children: [
+    { path: 'class', name: 'Class的基本语法', component },
+    { path: 'class-extends', name: 'Class的继承', component }
+  ]
+}
+
+export default ruanYiFeng
